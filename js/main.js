db0ffae0e4e67e7edd6f50054c36aaa34910d754
Davide <davide.roberti89@gmail.com>
2020-03-17 15:48:05 +0100

    fine layout

--- js/main.js
+++ js/main.js
@@ -7,6 +7,20 @@ $.ajax({
     success: function (data) {
         var libreriaAlbum = data.response;
         // console.log(libreriaAlbum);
+        for (var i = 0; i < libreriaAlbum.length; i++) {
+            var album = libreriaAlbum[i]
+            var insiemeAlbum = {
+                coverAlbum: album.poster,
+                titoloAlbum: album.title,
+                autoreAlbum: album.author,
+                annoAlbum: album.year,
+                genereAlbum: album.genre,
+            }
+            // console.log(insiemeAlbum);
+            var cardSingola = template(insiemeAlbum);
+            $('.card-container').append(cardSingola);
+            // console.log(cardSingola);
+        }
     },
     error: function () {
         alert('ERRORISSIMO!!!!');
