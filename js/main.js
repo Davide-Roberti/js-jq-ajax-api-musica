047a4d09a7453bd887e768ceb07e8d7af042280b
Davide <davide.roberti89@gmail.com>
2020-03-17 15:37:59 +0100

    chiamata ajax

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 var source = $("#card-template").html();
 var template = Handlebars.compile(source);
 
-$('.card-container').append(template);
+$.ajax({
+    url: 'https://flynn.boolean.careers/exercises/api/array/music',
+    method: 'GET',
+    success: function (data) {
+        var libreriaAlbum = data.response;
+        // console.log(libreriaAlbum);
+    },
+    error: function () {
+        alert('ERRORISSIMO!!!!');
+    }
+});
